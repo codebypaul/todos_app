510c16a7919820ac3fc40364b25760a7653af2e0
Paul Williams <paul.d.williamsxiv@gmail.com>
2023-11-04 13:29:36 -0400

    built template to view list of tasks and individual task

--- todos/app/urls.py
+++ todos/app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
-    path('',views.home_view, name='home view')
+    path('',views.home_view, name='home view'),
+    path('task-list/',TaskList.as_view()),
+    path('task-list/<int:pk>',TaskDetail.as_view())
 
 ]
