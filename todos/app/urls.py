35ff137ee97a28e4f7f8dafeb232cc3dd6ab7053
Paul Williams <paul.d.williamsxiv@gmail.com>
2023-11-04 18:59:47 -0400

    create link to view individual task

--- todos/app/urls.py
+++ todos/app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('',views.home_view, name='home view'),
-    path('task-list/',TaskList.as_view()),
-    path('task-list/<int:pk>',TaskDetail.as_view())
+    path('task-list/',TaskList.as_view(),name='task list'),
+    path('task/<int:pk>',TaskDetail.as_view(),name='task'),
+    path('task-create/',TaskCreate.as_view(),name='task create')
 
 ]
