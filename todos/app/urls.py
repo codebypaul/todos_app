03ff203675c97639e848b65343201ad349e78977
Paul Williams <paul.d.williamsxiv@gmail.com>
2023-11-05 11:56:12 -0500

    generated login and logout pages. connected functionality for users to sign-in and sign-out, restricted all app based views to require a authenticated user.

--- todos/app/urls.py
+++ todos/app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('',views.home_view, name='home view'),
+    # Auth views
+    path('sign-up/',CustomLoginView.as_view(), name='sign-up'),
+    path('login/',CustomLoginView.as_view(), name='login'),
+    path('logout/',LogoutView.as_view(next_page='home view'), name='logout'),
+    path('profile/',views.profile, name='profile'),
+    # App views
     path('task-list/',TaskList.as_view(),name='task list'),
     path('task/<int:pk>',TaskDetail.as_view(),name='task'),
     path('task-create/',TaskCreate.as_view(),name='task create'),
--- todos/app/views.py
+++ todos/app/views.py
@@ -3,36 +3,49 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
 from django.urls import reverse_lazy
-
+# Authentication
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+#Models
 from .models import Task
 
 # Create your views here.
-def home_view(request):
-    return HttpResponse('Todo List')
+class CustomLoginView(LoginView):
+    template_name = 'auth/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
 
-class TaskList(ListView):
+    def get_success_url(self):
+        return reverse_lazy('home view')
+    
+def home_view(request):
+    return render(request, 'home.html', context = {} ,status = 200)
+
+def profile(request):
+    return render(request,'profile.html',context = {}, status = 200)
+
+class TaskList(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin,DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'app/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('task list')
     # form_class = 'task_form'
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('task list')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('task list')
